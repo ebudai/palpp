bbe24ca84408eb128945a47506536fa548cb03c3
Eric Budai <eric_budai@hotmail.com>
2020-10-22 07:39:40 -0400

    initial commit

---
+++ include/platform.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+#include <chrono>
+
+namespace platform
+{
+	uint64_t available_memory();
+	uint64_t available_swap_space();
+	uint64_t used_swap_space();
+	void sleep(std::chrono::nanoseconds ns);
+}
---
+++ src/platform_windows.cpp
@@ -0,0 +1,9 @@
+#ifdef _WIN32
+#include "platform.h"
+#include <Windows.h>
+
+namespace platform
+{
+
+}
+#endif //_WIN32
